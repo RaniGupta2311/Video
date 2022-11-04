ca0074cefabd28dde00cfa8c862c4166220fed7b
Rani Gupta <rani.gupta259@gmail.com>
2022-11-04 16:05:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log("Welcome to video project");
+const btn=document.querySelector('.switch-btn-container');
+const video=document.querySelector(".video-container");
+btn.addEventListener('click',function(){
+    if(!btn.classList.contains("slide")){
+        btn.classList.add('slide');
+        video.pause();
+    }
+    else{
+        btn.classList.remove('slide');
+        video.play();
+    }
+});
+
+// preloader
+const preloader=document.querySelector(".preloader");
+window.addEventListener("load",function(){
+    preloader.classList.add("hide-preloader");
+})
